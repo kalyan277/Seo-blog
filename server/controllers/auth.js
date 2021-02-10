1caa065b68092fd71dd916398f09a7f167cec749
unknown <kalyansingh277@gmail.com>
2021-02-10 08:00:42 +0530

    fixed the mailer repeat

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -13,7 +13,6 @@ const mailTransporter = nodemailer.createTransport(
     host: 'smtp.gmail.com',
     port: 587,
     secure: false,
-    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
