9be6e726c9daeb09d94b1948913205cd4ee8276f
unknown <kalyansingh277@gmail.com>
2021-02-10 08:09:07 +0530

    fixed the mailer repeat

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -7,19 +7,14 @@ const { errorHandler } = require('../helper/dbErrorHandler');
 const nodemailer = require('nodemailer');
 const _ = require('lodash');
 
-const mailTransporter = nodemailer.createTransport(
+const mailTransporter = nodemailer.createTransport("SMTP",
 {
     service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-      tls: {
-      rejectUnauthorized: false
-    },
+  
     
 });
 
